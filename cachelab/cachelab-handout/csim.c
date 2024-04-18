466ded7b6b120d6d8e27f702b0dc1c89baf18216
1328032567 <1328032567@qq.com>
2024-04-18 22:00:21 +0800

    little change

--- cachelab/cachelab-handout/csim.c
+++ cachelab/cachelab-handout/csim.c
@@ -2,8 +2,16 @@
 //csim -> cache simulator
 //Xuan UESTC
 
+void printSummary(hit_count, miss_count, eviction_count);
+
 int main()
 {
     printSummary(0, 0, 0);
     return 0;
 }
+
+void printSummary(hit_count, miss_count, eviction_count)
+{
+
+}
+

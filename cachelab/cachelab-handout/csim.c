0d49e55df22f22658c4fcb1a03f5832c62006ebe
xuan <1328032567@qq.com>
2024-04-29 15:26:09 +0800

    cache simulator finished

--- cachelab/cachelab-handout/csim.c
+++ cachelab/cachelab-handout/csim.c
@@ -1,8 +1,13 @@
 #include "cachelab.h"
 #include<stdio.h>
+#include<string.h>
 #include<stdlib.h>
 #include<math.h>
+#include<time.h>
 #define SIZE 50
+int hit = 0;
+int miss = 0;
+int evic = 0;
 //csim -> cache simulator
 //Xuan UESTC
 
@@ -41,23 +46,24 @@ int main(int argc, char* argv[])
 
     // printf("s:%d e:%d b:%d t:%s \n", s, E, b, fname);
     int S = pow(2, s);
-    int B = pow(2, b);
-    int t = 32 - (b + s);
-    char ***cache = (char*** )malloc(sizeof(char) * S * E * (t + 1));
+    // int B = pow(2, b);
+    // int t = 32 - (b + s);
 
-    //Initialize cache
-    for(int i = 0;i < S;i++)
+    //malloc cache array
+    int*** cache = (int*** )malloc(S * sizeof(int**));
+    for (int i = 0; i < S; i++) 
     {
-        for(int j = 0;j < E;j++)
-            *(cache + i*E*(t+1) + j*(t+1))= 0;
+        cache[i] = (int** )malloc(E * sizeof(int*));
+        for (int j = 0; j < E; j++) 
+        {
+            cache[i][j] = (int* )malloc(2 * sizeof(int));
+            cache[i][j][0] = 0;
+        }
     }
 
     //handle the stdin command
     char cmd[SIZE];
     //initialize hit, miss, eviction
-    int hit = 0;
-    int miss = 0;
-    int evic = 0;
     while((fgets(cmd, SIZE, fp)) != NULL && cmd[0] != '\n')
     {
         char* ad;
@@ -77,7 +83,7 @@ int main(int argc, char* argv[])
             ad = cmd + 3;
             int at, as;
             int num = hextodec(ad, length-1);
-            printf("num:%d\n", num);
+//            printf("num:%d\n", num);
             //handle the t and s
             if(4*length <= b)
             {
@@ -93,12 +99,114 @@ int main(int argc, char* argv[])
                 num >>= b;
                 int tmp = num;
                 at = num >> s;
-                as = tmp % (as<<s);
+                as = tmp % (at<<s);
             }
-            printf("t:%d s:%d \n", at, as);
+            //replace the '\n' into '\0'
+            char *ch = strchr(cmd, '\n');
+            if (ch != NULL) {
+                *ch = '\0';
+            }
+//            printf("t:%d s:%d \n", at, as);
+            if(as < S)
+            {
+                for(int i = 0;i < E;i++)
+                {
+                    int effectbit;
+                    int tbit;
+                    int emptyline = -1;
+                    int flag1 = 0;//emptyline
+                    int flag2 = 0;//hitflag
+                    char ch = cmd[1];
+                    if(mode == 1)
+                    {
+                        fprintf(stdout, "%s ", cmd);
+                    }
+                    if((effectbit = cache[as][i][0]) == 1)//effective line
+                    {
+                        if((tbit = cache[as][i][1]) == at)
+                        {
+                            //hit
+                            hit++;
+                            if(mode == 1)
+                                fprintf(stdout, "%s ", "hit");
+                            if(ch == 'M')
+                            {
+                                if(mode == 1)
+                                    fprintf(stdout, "%s", "hit");
+                                hit++;
+                            }
+                            flag2 = 1;
+                            break;
+                        }
+                    }
+                    else //emptyline
+                    {
+                        if(flag1 == 0)//first emptyline
+                        {
+                            emptyline = i;//mark the emptyline
+                            flag1 = 1;//own the emptyline
+                        }
+                    }
+                    if(flag2 == 0)
+                    {
+                        //miss;
+                        miss++;
+                        if(mode == 1)
+                            fprintf(stdout, "%s ", "miss");
+                        if(ch == 'L' || ch == 'S' || ch == 'M')
+                        {
+                            //Load data or Store data
+                            if(flag1 == 1)//own the empty line
+                            {
+                                cache[as][emptyline][0] = 1;//effect the line
+                                cache[as][emptyline][1] = at;
+                            }
+                            else//no emptyline
+                            {
+                                //eviction
+                                evic++;
+                                srand(time(0));
+                                int line = rand() % E;
+                                cache[as][line][1] = at;
+                                if(mode == 1)
+                                    fprintf(stdout, "%s ", "eviction");
+                            }
+                        }
+                        if(ch == 'M')
+                        {
+                            if(mode == 1)
+                                fprintf(stdout, "%s", "hit");
+                            hit++;
+                        }
+                        // else if(ch == 'S')
+                        // {
+                        //     //Store data
+                        // }
+                        // else if(ch == 'M')
+                        // {
+                        //     //Modify data
+                        // }
+                    }
+                }
+            }
+            else
+            {
+                fprintf(stderr, "Wrong Group Number!");
+            }
+            fprintf(stdout, "\n");
         }
     }
-    printSummary(1, 2, 3);
+    printSummary( hit, miss, evic);
+    //free cache array
+    for (int i = 0; i < S; i++) {
+        for (int j = 0; j < E; j++) {
+            free(cache[i][j]);
+        }
+        free(cache[i]);
+    }
+    free(cache);
+    //close file fp
+    fclose(fp);
     return 0;
 }
 

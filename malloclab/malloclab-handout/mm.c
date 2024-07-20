eabcba35e203b25263d74a32d27cfe4cc0cca940
xuan <1328032567@qq.com>
2024-07-20 14:41:41 +0800

    <modify>:add team related information.

--- malloclab/malloclab-handout/mm.c
+++ malloclab/malloclab-handout/mm.c
@@ -24,11 +24,11 @@
  ********************************************************/
 team_t team = {
     /* Team name */
-    "ateam",
+    "MallocTeam",
     /* First member's full name */
-    "Harry Bovik",
+    "Xuan",
     /* First member's email address */
-    "bovik@cs.cmu.edu",
+    "xuan@edu",
     /* Second member's full name (leave blank if none) */
     "",
     /* Second member's email address (leave blank if none) */

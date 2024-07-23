47644536e3720952b49a6a11f5bd94af772c1f68
xuan <1328032567@qq.com>
2024-07-23 15:05:52 +0800

    <update>:update the initialize function of segregated fit.

--- malloclab/malloclab-handout/mm.c
+++ malloclab/malloclab-handout/mm.c
@@ -52,25 +52,39 @@ team_t team = {
 #define MAX(x, y)       ((x) > (y) ? (x) : (y))
 
 /* Pack a size and allocated bit into a word */
-/* size -> header + payload + footer, alloc -> block whether allocated*/
-#define PACK(size, alloc)   ((size) | (alloc))
+/* size -> header + payload + footer, prev_alloc -> previous block whether allocated, alloc -> block whether allocated*/
+#define PACK(size, prev_alloc, alloc)   ((size) | (prev_alloc) | (alloc))
 
 /* Read and write a word at address p */
 #define GET(p)          (*(unsigned int *)(p))
 #define PUT(p, val)     (*(unsigned int *)(p) = (val))
 
 /* Read the size and allocated fields from address p */
-#define GET_SIZE(p)     (GET(p) & ~0x7)     /* Sum of header, footer and payload */
-#define GET_ALLOC(p)    (GET(p) & 0x1)
+#define GET_SIZE(p)         (GET(p) & ~0x7)     /* Sum of header, footer and payload */
+#define GET_ALLOC(p)        (GET(p) & 0x1)
+#define GET_PREV_ALLOC(p)   (GET(p) & 0x2)
 
 /* Given block ptr bp, compute address of its header and footer */
 #define HDRP(bp)        ((char *)(bp) - WSIZE)
 #define FTRP(bp)        ((char *)(bp) + GET_SIZE(HDRP(bp)) - DSIZE)
 
 /* Given block ptr bp, compute address of next and previous blocks */
-#define NEXT_BLKP(bp)   ((char *)(bp) + GET_SIZE(((char *)(bp) - WSIZE))) 
+#define NEXT_BLKP(bp)   ((char *)(bp) + GET_SIZE(((char *)(bp)))) 
 #define PREV_BLKP(bp)   ((char *)(bp) - GET_SIZE(((char *)(bp) - DSIZE)))
 
+/* Given block ptr bp, compute address of next and previous nodes in the list*/
+#define NEXT_NODE(bp)   ((char *)(mem_heap_lo() + *(unsigned int *)((bp) + WSIZE)))
+#define PREV_NODE(bp)   ((char *)(mem_heap_lo() + *(unsigned int *)(bp)))
+
+/* Given block ptr bp, set previous node and next node address */
+#define SET_NEXT_NODE(bp, addr)     (*(unsigned int *)((bp) + WSIZE) = (unsigned int)(addr))
+#define SET_PREV_NODE(bp, addr)     (*(unsigned int *)(bp) = (unsigned int)(addr))
+
+/* Define the number of the free_list */
+#define FREE_LIST_NUM 15
+
+/* Define gobal free list to store head pointer */
+static char **free_list;
 /* Additional Functions*/
 static void *extend_heap(size_t words);
 static void *coalesce(void *bp);
@@ -84,13 +98,21 @@ static void *heap_listp;
  */
 int mm_init(void)
 {
-    /* Create the initial empty heap */
+    /* Initialize free_list position */
+    free_list = mem_heap_lo();
+    for(int i = 0; i < FREE_LIST_NUM; i++){
+        if((heap_listp = mem_sbrk(WSIZE)) == (void *)(-1))
+            return -1;
+        free_list[i] = mem_heap_lo();
+    }
+
+    /* Create the prologue header, footer and epilogue header at heap */
     if((heap_listp = mem_sbrk(4*WSIZE)) == (void *)-1)
         return -1;
     PUT(heap_listp, 0);                             /* Alignment padding */
-    PUT(heap_listp + (1*WSIZE), PACK(DSIZE, 1));    /* Prologue header */
-    PUT(heap_listp + (2*WSIZE), PACK(DSIZE, 1));    /* Prologue footer */
-    PUT(heap_listp + (3*WSIZE), PACK(0, 1));        /* Epilogue header */
+    PUT(heap_listp + (1*WSIZE), PACK(DSIZE, 0, 1));    /* Prologue header */
+    PUT(heap_listp + (2*WSIZE), PACK(DSIZE, 0, 1));    /* Prologue footer */
+    PUT(heap_listp + (3*WSIZE), PACK(0, 0, 1));        /* Epilogue header */
     heap_listp += (2*WSIZE);
 
     /* Extend the empty heap with a free block of CHUNKSIZE bytes */

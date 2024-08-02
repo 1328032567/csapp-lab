a5bb42ae10d32020ae5e262f224798e4dab6e03d
xuan <1328032567@qq.com>
2024-08-02 15:36:49 +0800

    <add>:add new sbuf relative file to workspace.

---
+++ proxylab/proxylab-handout/sbuf.h
@@ -0,0 +1,9 @@
+typedef struct {
+    int *buf;       /* Buffer arrary */
+    int n;          /* Maximum number of slots */
+    int front;      /* buf[(front+1)%n] is first item */
+    int rear;       /* buf[rear%n] is last item */
+    sem_t mutex;    /* Protects accesses to buf */
+    sem_t slots;    /* Counts available slots */
+    sem_t items;    /* Counts available items */
+} sbuf_t;
